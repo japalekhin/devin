95d72046cc7c733e6e130fb5f583f1ff78bbc5d5
Japa Alekhin Llemos <alekhin@llemos.com>
2017-07-04 11:35:17 +0800

    - fixed reverse auto scroll

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@
     mis.init = function () {
         var ww = $(window).width();
         var wh = $(window).height();
-        if (ww < 768 && $('body').hasClass('home')) {
+        if (ww < 768 && !$('body').hasClass('home')) {
             $('.site-brand .site-header .site-' + (ww < wh ? 'social' : 'support'))[0].scrollIntoView();
         }
     };
